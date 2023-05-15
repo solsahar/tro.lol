b1bd320218122f5dfbad03939d3dfbb0bb804e93
Solsa <32788382+solsahar@users.noreply.github.com>
2023-05-15 12:40:06 -1000

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
-from flask import Flask, render_template
-
-app = Flask(__name__)
-
-@app.route('/')
-def home():
+from gevent import monkey
+monkey.patch_all()
+from http import server
+from flask import request
+from flask import redirect
+from flask import Flask, json, render_template
+from flask import send_file
+import gevent.pywsgi
+import json
+import subprocess
+website = Flask(__name__)
+@website.route('/',methods=['GET'])
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app_server = gevent.pywsgi.WSGIServer(('0.0.0.0',443), website)
+    app_server.serve_forever()
