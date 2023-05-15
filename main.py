eeade8aa40cc01a1c50436106c7659c1ed8428ad
Solsa <32788382+solsahar@users.noreply.github.com>
2023-05-15 12:46:05 -1000

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
 from gevent import monkey
 monkey.patch_all()
 from http import server
-from flask import request
-from flask import redirect
-from flask import Flask, json, render_template
-from flask import send_file
-import gevent.pywsgi
+from flask import request, redirect, Flask, json, render_template, send_file
+from gevent import pywsgi
 import json
 import subprocess
+
 website = Flask(__name__)
-@website.route('/',methods=['GET'])
+
+@website.route('/', methods=['GET'])
 def trolol():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app_server = gevent.pywsgi.WSGIServer(('0.0.0.0',443), website)
+    app_server = pywsgi.WSGIServer(('0.0.0.0', 443), website)
     app_server.serve_forever()
